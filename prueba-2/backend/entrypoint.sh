1756f3ff6e6f7fca0afcc2572eac42a84d78b35c
Lautaro Rodriguez <lautaro.rodriguez@mi.unc.edu.ar>
2024-05-31 16:48:28 -0300

    feat: add is_staff atribute to custom user model, then create migrations and create super user when entrypoint is executed

--- prueba-2/backend/entrypoint.sh
+++ prueba-2/backend/entrypoint.sh
@@ -14,10 +14,31 @@ echo "Database is ready, proceeding with Django commands."
 
 # We have base custom user model so need to makemigrations out of box
 
-python manage.py makemigrations core
+python manage.py makemigrations
 python manage.py flush --no-input
 python manage.py migrate
 python manage.py loaddata initial_data.json
+
+# Create superuser
+echo "Creating superuser..."
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = '$SUPERUSER_USERNAME'
+email = '$SUPERUSER_EMAIL'
+password = '$SUPERUSER_PASSWORD'
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f"Superuser {username} created successfully!")
+else:
+    print(f"Superuser {username} already exists.")
+EOF
+
+echo "Starting Django server..."
+
+
 # python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
 
